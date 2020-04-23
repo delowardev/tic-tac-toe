ec12ad636820251d1f19780bc5e72eeae165a89b
Delowar <delowardev@gmail.com>
2020-04-23 20:49:58 +0600

    background image added

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -7,5 +7,10 @@
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Fondamento:400,400i&display=swap');
   @import "./styles/_normalize.scss";
+
+  :root {
+    --body-bg: url('./assets/background.jpg');
+  }
+
   @import "./styles/style.scss";
 </style>
